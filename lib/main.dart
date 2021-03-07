c101295e5ffacdef876f2325e8af02328b7192d2
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-03-07 22:56:23 +0300

    Ekleme, Silme ve Güncelleme butonlarının işlemleri

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,23 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
-  }
+}
 
 class _MyAppState extends State<MyApp> {
   String mesaj = "Öğrenci Takip Sistemi";
 
-  String seciliOgrenci= "abc";
+  String seciliOgrenci = "abc";
 
-  List <Student> students = [Student("Engin","Demiroğ",25),Student("Kerem","Varış",65),Student("Halil ","Duymaz",45)];
+  List<Student> students = [
+    Student("Engin", "Demiroğ", 25),
+    Student("Kerem", "Varış", 65),
+    Student("Halil ", "Duymaz", 45)
+  ];
 
-  var ogrenciler = [
-    "Engin Demirog ",
-    "Kerem Varıs",
-    "Berkay Bilgin"];
+  var ogrenciler = ["Engin Demirog ", "Kerem Varıs", "Berkay Bilgin"];
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
       appBar: AppBar(
         title: Text(mesaj),
@@ -38,52 +37,108 @@ class _MyAppState extends State<MyApp> {
                   itemBuilder: (BuildContext context, int index) {
                     return ListTile(
                       leading: CircleAvatar(
-                        backgroundImage: NetworkImage("https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png"),
+                        backgroundImage: NetworkImage(
+                            "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png"),
                       ),
-                      title: Text(students[index].firsName + " "+ students[index].lastName),
-                      subtitle:Text("sınavdan aldığı not: "+ students[index].grade.toString()+" ["+students[index].getStatus+"]"),
+                      title: Text(students[index].firsName +
+                          " " +
+                          students[index].lastName),
+                      subtitle: Text("sınavdan aldığı not: " +
+                          students[index].grade.toString() +
+                          " [" +
+                          students[index].getStatus +
+                          "]"),
                       trailing: buildStatusIcon(students[index].grade),
-                      onTap: (){
-                       setState(() {
-                         seciliOgrenci = students[index].firsName + " "+ students[index].lastName;
-                       });
+                      onTap: () {
+                        setState(() {
+                          seciliOgrenci = students[index].firsName +
+                              " " +
+                              students[index].lastName;
+                        });
                         print(seciliOgrenci);
-
                       },
                     );
                   })),
-          Text("Seçili Öğrenci: "+ seciliOgrenci),
-          Center(
-            child: RaisedButton(
-              child: Text("Sonuc"),
-              onPressed: () {
-                var mesaj = sinavHesapla(55);
-                mesajGoster(context, mesaj);
-              },
-            ),
-          ),
+          Text("Seçili Öğrenci: " + seciliOgrenci),
+          Row(
+            children: <Widget>[
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 2,
+                child: RaisedButton(
+                  color : Colors.greenAccent,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.add),
+                        SizedBox(width: 5.0,),
+                        Text("Yeni Öğrenci"),
+                      ],
+                    ),
+                    onPressed: () {
+                      var mesaj = sinavHesapla(55);
+                      mesajGoster(context, mesaj);
+                    },
+                  ),
+
+                ),
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 2,
+                child: RaisedButton(
+                  color : Colors.yellowAccent,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.update),
+                      SizedBox(width: 5.0,),
+                      Text("Güncelle"),
+                    ],
+                  ),
+                  onPressed: () {
+                    var mesaj = sinavHesapla(55);
+                    mesajGoster(context, mesaj);
+                  },
+                ),
+
+              ),
+              Flexible(
+                fit: FlexFit.tight,
+                flex: 1,
+                child: RaisedButton(
+                  color : Colors.redAccent,
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.delete),
+                      SizedBox(width: 5.0,),
+                      Text("Sil"),
+                    ],
+                  ),
+                  onPressed: () {
+                    var mesaj = sinavHesapla(55);
+                    mesajGoster(context, mesaj);
+                  },
+                ),
+
+              ),
+            ],
+          )
         ],
       ),
     );
   }
 
   String sinavHesapla(int puan) {
-String mesaj = "";
-if(puan>=50){
-  mesaj="Geçti";
-  }
- else if (puan >=40){
-   mesaj="Bütünlemeye Kaldı";
- }
- else {
-   mesaj="Kaldi";
-
- }
- return mesaj;
-
+    String mesaj = "";
+    if (puan >= 50) {
+      mesaj = "Geçti";
+    } else if (puan >= 40) {
+      mesaj = "Bütünlemeye Kaldı";
+    } else {
+      mesaj = "Kaldi";
+    }
+    return mesaj;
   }
 
-  void mesajGoster(BuildContext context,String mesaj) {
+  void mesajGoster(BuildContext context, String mesaj) {
     var alert = AlertDialog(
       title: Text("Sinav Sonucu"),
       content: Text(mesaj),
@@ -94,7 +149,7 @@ if(puan>=50){
 
   Widget buildStatusIcon(int grade) {
     if (grade >= 50) {
-       return Icon(Icons.done);
+      return Icon(Icons.done);
     } else if (grade >= 40) {
       return Icon(Icons.album);
     } else {
@@ -102,4 +157,3 @@ if(puan>=50){
     }
   }
 }
-
